4b0c7a355125390f73ec8ef3207ddd5afdf3f448
Miguel Expósito <exposito_m@cantabria.es>
2016-01-26 21:06:17 +0100

    Modificada configuración cmake y gitignore

---
+++ CMakeFiles/jugador.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jugador.dir/jugador.c.o"
+  "CMakeFiles/jugador.dir/mus.c.o"
+  "jugador.pdb"
+  "jugador"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jugador.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
